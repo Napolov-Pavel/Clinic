25085e64a7864deeb60b0a828b33fc9be6106984
Napolov Pavel <Napolov.pavel@yandex.ru>
2022-03-14 18:44:51 +0300

    init commit

--- src/main/java/com/example/clinic/ClinicApplication.java
+++ src/main/java/com/example/clinic/ClinicApplication.java
@@ -1,13 +1,23 @@
 package com.example.clinic;
 
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClinicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClinicApplication.class, args);
-	}
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+    @Bean
+    public LayoutDialect layoutDialect() { return new LayoutDialect();}
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicApplication.class, args);
+    }
 
 }
